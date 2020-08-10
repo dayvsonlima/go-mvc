96b87d92ae1633555eac0a8dfed23ca957834207
Dayvson Lima <dayvsonlima31@gmail.com>
2020-08-10 01:09:54 -0300

    Add delete method

--- app/controllers/posts/delete.go
+++ app/controllers/posts/delete.go
@@ -1,8 +1,21 @@
 package posts
 
-import "github.com/gin-gonic/gin"
+import (
+	"application/app/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // PostsDelete .
 func Delete(ctx *gin.Context) {
+	var post models.Post
+	if err := db.Where("id = ?", ctx.Param("id")).First(&post).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
 
+	db.Delete(&post)
+
+	ctx.JSON(http.StatusOK, gin.H{"data": true})
 }
